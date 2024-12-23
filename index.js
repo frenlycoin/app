17a2839e04cb2a8a0e6c9f02b35145a67c2159f8
Frenly Bro <frenlybro@proton.me>
2024-12-23 17:02:32 +0100

    put TON in counter

--- index.js
+++ index.js
@@ -266,6 +266,10 @@ class App {
         var earnings = app.getRewards();
         app.updateProgress();
         $("#earnings").html(earnings);
+        
+        var r = app.getRewards();
+        $("#earningsth").html((earnings / 10).toFixed(9));
+
         app.tmout = setTimeout(app.countEarnings, 1000);
     }
 
